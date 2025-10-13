62ab3775b7586f063a9a73bd097f323ef2b1f89d
Domenico <34346930+domenico-somma@users.noreply.github.com>
2025-10-13 23:01:24 +0100

    Update run_slurm.sh
    More generic

--- dist/run_slurm.sh
+++ dist/run_slurm.sh
@@ -1,27 +1,23 @@
 #!/bin/bash
 
 ############# SLURM SETTINGS #############
-#SBATCH --account=project0001
-#SBATCH --job-name=R_pipeline
-#SBATCH --output=R_pipeline-%j.out
-#SBATCH --error=R_pipeline-%j.err
-#SBATCH --partition=short
+#SBATCH --job-name=scReady
+#SBATCH --output=scReady-%j.out
+#SBATCH --error=scReady-%j.err
 #SBATCH --time=02:00:00
 #SBATCH --mem=100G
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
-#SBATCH --cpus-per-task=5
+#SBATCH --cpus-per-task=1
 #SBATCH --ntasks-per-node=1
 
 ############# LOADING MODULES (optional) #############
-module load apps/apptainer
+# module load apps/apptainer
 
 ############# MY CODE #############
 #
 # RUN:
 # sbatch this_script.sh folder_path <optional_metadata_file.csv>
-#
-# folder_path need to be a folder containing cellranger mappings
 
 IMAGE="docker://ghcr.io/sii-scrna-seq/scready:latest"
 
